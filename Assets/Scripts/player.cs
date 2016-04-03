274c338e42efae795904b598c2146b6ca24a88c1
岩崎謙汰 <iwaken59371518@gmail.com>
2016-04-03 18:59:09 +0900

    2

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class player : Photon.MonoBehaviour {
+	
 	public GameObject bullet;
 	public Transform muzzle;
 	public float speed = 20;
@@ -13,41 +14,51 @@ public class player : Photon.MonoBehaviour {
 	public Text HPText;
 	// Use this for initialization
 	void Start () {
-		Hp = GameObject.Find ("Image");
+		if (photonView.isMine) {
+			Hp = GameObject.Find ("Image");
+		}
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			GameObject obj = PhotonNetwork.Instantiate ("bullet", muzzle.position, transform.rotation,0) as GameObject;
-			Rigidbody rd = obj.GetComponent<Rigidbody> ();
-			rd.velocity = this.transform.forward * speed;
-		}
+		if (photonView.isMine) {
+			Hp.gameObject.GetComponent<Image> ().fillAmount = HP / 20.0f;
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				GameObject obj = PhotonNetwork.Instantiate ("bullet", muzzle.position, transform.rotation, 0) as GameObject;
+				Rigidbody rd = obj.GetComponent<Rigidbody> ();
+				rd.velocity = this.transform.forward * speed;
+			}
 
-		if (HP <= 0) {
-			this.transform.position = new Vector3(-5.15f,-16f,-3.38f);
-			flag = 1;
-		}
+			if (HP <= 0) {
+				this.transform.position = new Vector3 (-5.15f, -16f, -3.38f);
+				flag = 1;
+			}
 
-		if (flag == 1) {
-			timer += Time.deltaTime;
-		}
+			if (flag == 1) {
+				timer += Time.deltaTime;
+			}
 
-		if (timer >= 7) {
-			time ();
+			if (timer >= 7) {
+				time ();
+			}
 		}
 	
 	}
 	void damage (){
-		HP -= 5;
-		Hp.gameObject.GetComponent<Image> ().fillAmount -= 0.05f;
+		if (photonView.isMine) {
+			HP -= 5;
+			Hp.gameObject.GetComponent<Image> ().fillAmount = HP / 20.0f;
+		}
 	}
 	void time (){
-		Hp.gameObject.GetComponent<Image> ().fillAmount = 1;
-		flag = 0;
-		timer = 0;
-		this.transform.position = new Vector3 (0,5,-2);
+		if (photonView.isMine) {
+			Hp.gameObject.GetComponent<Image> ().fillAmount = 1;
+			HP = 100;
+			flag = 0;
+			timer = 0;
+			this.transform.position = new Vector3 (0, 5, -2);
+		}
 	}
 	/*void OnCollisionEnter (Collision col){
 		if(col.gameObject.tag == player){
