6128d025cbc9848a4c3da47827069bfa935d1284
岩崎謙汰 <iwaken59371518@gmail.com>
2016-04-03 11:25:13 +0900

    tanaka

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+	private float timer = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= 7) {
+			Destroy (this.gameObject);
+		} 
+	
+	}
+	void OnCollisionEnter (Collision col){
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.SendMessage("damage");
+		}
+	}
+}
